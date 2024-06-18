f02b3276f73c1b45caebacf5dd022b4ecd1e71c3
Harsh Telang <telangharsh06@gmail.com>
2024-06-18 21:21:27 +0530

    feat - Campaign component completed

--- src/app/components/Campaign.tsx
+++ src/app/components/Campaign.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import localFont from "next/font/local";
 import { CAMPAIGNS } from "@/utils/data";
-import { motion } from "framer-motion";
+import { CardStack } from "./Campaign/card-stack";
 
 const panchang = localFont({
   src: "../../../public/Panchang-Variable.ttf",
@@ -10,49 +10,28 @@ const panchang = localFont({
 
 const Campaign = () => {
   return (
-    <div className="w-screen h-[70vh] flex justify-between items-center mb-14">
-      <div className="flex items-center h-full gap-10">
-        <div className="h-[45vh] flex justify-center items-center">
+    <div className="w-screen sm:h-[70vh] h-[50vh] flex sm:flex-row flex-col sm:justify-between sm:items-center sm:mb-14 sm:pt-0 pt-10">
+      <div className="flex items-center sm:h-full md:gap-10 sm:gap-5">
+        <div className="lg:h-[45vh] sm:h-[31vh] h-20 flex justify-center items-center">
           <img
             src="/CREATIVE TEXTURED GLASS FOR ARCHITECTS _ Float Glass Design for Architects and Textured and Creative Glass Applications 1.png"
             alt="effect"
-            className="h-full w-14"
+            className="h-full md:w-14 sm:w-7 w-5"
           />
         </div>
         <div
-          className={`h-[45vh] w-40 flex justify-center items-center break-words ${panchang.className}`}
+          className={`sm:h-[45vh] md:w-40 sm:w-32 flex md:justify-center items-center break-words ${panchang.className}`}
         >
-          <h1 className=" bg-gradient-to-br from-[#283BF8] to-[#B8BEFF] text-transparent bg-clip-text font-semibold text-7xl leading-[1.2] tracking-[0.1em] w-52">
+          <h1 className=" bg-gradient-to-br from-[#283BF8] to-[#B8BEFF] text-transparent bg-clip-text font-semibold lg:text-7xl md:text-6xl sm:text-xl text-base leading-[1.2] tracking-[0.1em] w-52">
             CAMPAIGNS
           </h1>
         </div>
       </div>
-      <div className="h-full w-[70%] flex">
-        <div className="bg-white h-40 w-7 rounded mt-20"></div>
-        <div className="h-full w-[75%] px-10 flex items-center relative overflow-hidden">
-          <motion.div 
-            className="h-[60vh] w-80 bg-white rounded-3xl flex justify-center items-center overflow-hidden"
-            whileInView={{ x: [-300, 0], opacity: [0, 1] }}
-            transition={{ duration: 1, ease: "easeOut" }}
-          >
-            <img src={CAMPAIGNS[0].lineImg} alt="camImg" className="size-auto"/>
-            {/* <img src={CAMPAIGNS[0].selectImg} alt="camImg" className="size-auto hidden hover:flex"/> */}
-          </motion.div>
-          <motion.div 
-            className="h-[55vh] w-80 bg-white rounded-3xl absolute flex justify-center items-center overflow-hidden right-[10rem] bottom-8 -z-10 opacity-30"
-            whileInView={{ x: [-500, 0], opacity: [0, 0.3] }}
-            transition={{ duration: 1.2, ease: "easeOut" }}
-          >
-            <img src={CAMPAIGNS[1].lineImg} alt="camImg" className="size-full"/>
-          </motion.div>
-          <motion.div 
-            className="h-[50vh] w-80 bg-red-200 rounded-3xl absolute flex justify-center items-center overflow-hidden bottom-8 -right-[2rem] -z-20 opacity-5"
-            whileInView={{ x: [-700, -90], opacity: [0, 0.05] }}
-            transition={{ duration: 1.4, ease: "easeOut" }}
-          >
-            {/* <img src={CAMPAIGNS[2].lineImg} alt="camImg" className="size-full"/> */}
-          </motion.div>
-          <div className="absolute h-24 overflow-hidden w-full bg-gradient-radial from-[#03062A] to-[#0F165C] rounded-b-[50%] -z-30 -bottom-0 right-8"></div>
+      <div className="sm:h-full md:w-[70%] w-[26rem] flex">
+        <div className="bg-white lg:h-40 md:h-20 w-7 rounded lg:mt-20 md:mt-28 md:flex hidden"></div>
+        <div className="h-full w-[75%] md:pl-10 pl-36 sm:pr-10 flex relative md:overflow-hidden lg:mt-10 md:mt-24 sm:mt-32 mt-6">
+          <CardStack items={CAMPAIGNS} offset={100} scaleFactor={0.1}/>
+          {/* <div className="absolute h-24 overflow-hidden :w-full bg-gradient-radial from-[#03062A] to-[#0F165C] rounded-b-[50%] -z-30 bottom-11 right-0"></div> */}
         </div>
       </div>
     </div>
---
+++ src/app/components/Campaign/card-stack.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+let interval: any;
+
+type Card = {
+  id: number;
+  lineImg: string;
+  selectImg: string;
+};
+
+export const CardStack = ({
+  items,
+  offset,
+  scaleFactor,
+}: {
+  items: Card[];
+  offset?: number;
+  scaleFactor?: number;
+}) => {
+  const CARD_OFFSET = offset || 10;
+  const SCALE_FACTOR = scaleFactor || 0.06;
+  const [cards, setCards] = useState<Card[]>(items);
+
+  useEffect(() => {
+    startFlipping();
+
+    return () => clearInterval(interval);
+  }, []);
+  const startFlipping = () => {
+    interval = setInterval(() => {
+      setCards((prevCards: Card[]) => {
+        const newArray = [...prevCards];
+        newArray.unshift(newArray.pop()!);
+        return newArray;
+      });
+    }, 10000);
+  };
+
+  return (
+    <div className="relative h-[60vh] lg:w-80 md:h-60 md:w-60 flex justify-center">
+      {cards.map((card, index) => {
+        return (
+          <motion.div
+            key={card.id}
+            className="absolute sm:h-60 h-52 sm:w-60 w-32 lg:h-[60vh] md:h-72 lg:w-80 md:w-52 rounded-3xl overflow-hidden shadow-2xl shadow-[#03062A] flex flex-col justify-between"
+            animate={{
+              right: index * -CARD_OFFSET,
+              scale: 1 - index * SCALE_FACTOR,
+              zIndex: cards.length - index,
+            }}
+          >
+            <img src={card.lineImg} alt="camImg" className="size-full"/>
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+};
