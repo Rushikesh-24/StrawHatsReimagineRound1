3178c7e745ec01c4d638e1b6a6216822845d2c35
Shridhar <shridharkamat10@gmail.com>
2024-06-24 20:42:30 +0530

    feat: added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ import Moo from "./components/Moo";
 import { TextGenerateEffect } from "./components/TextGenerate";
 import MilkMan from "./components/MilkMan";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
   return (
-    <main className=" max-w-[2000px] mx-auto overflow-hidden">
+    <main className=" max-w-[2000px] h-screen relative mx-auto overflow-x-hidden">
+      <Navbar />
       <Hero/>
       <Moo/>
       <Products />
       <OurStory />
       <MilkMan/>
-    <Campaign />
+      <Campaign />
       <Footer/>
     </main>
   );
